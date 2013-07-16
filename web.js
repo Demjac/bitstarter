e2752f270d6cd6a582093ab8f018663dec9293b2
Demjac <Demonesha.Jackson@gmail.com>
2013-07-16 05:41:00 +0000

    Updated web.js thrice

--- web.js
+++ web.js
@@ -1,16 +1,14 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
 
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 var readerf = fs.readFileSync("index.html");
 var data = buf.toString('utf8', readerf);
 app.get('/', function(request, response) {
-  response.send(data);
+var fs = require('fs');
+var buffer = new buffer();
+  response.send("utf-8", data);
 });
 
 var port = process.env.PORT || 5000;
