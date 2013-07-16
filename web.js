fbcae5c379feda2647a86af19e2b61266c1dbf79
Demjac <Demonesha.Jackson@gmail.com>
2013-07-16 06:20:12 +0000

    Updated web.js nine

--- web.js
+++ web.js
@@ -6,9 +6,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
 var readerf = fs.readFileSync('index.html');
-var data = readerf.toString('utf8', readerf);
-var bfr = new Buffer(data);
-  response.send(bfr);
+var buf = new Buffer(data);
+var data = buf.toString('utf8', readerf);
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
