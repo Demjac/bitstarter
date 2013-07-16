e5b01220a9b04d44465bc5ad1c7c3d08f169a88e
Demjac <Demonesha.Jackson@gmail.com>
2013-07-16 06:55:49 +0000

    Updated web.js twelve

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
 var buffer = new Buffer(256, 'utc-8');
-  response.send(buffer.toString(fs.readFileSync("index.html, 'utc-8')));
+  response.send(buffer.toString(fs.readFileSync("index.html", 'utc-8')));
 });
 
 var port = process.env.PORT || 5000;
