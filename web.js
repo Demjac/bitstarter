d38020d93f8ad07e1db48a8829670f7ea055cae0
Demjac <Demonesha.Jackson@gmail.com>
2013-07-16 05:53:50 +0000

    Updated web.js four

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var readerf = fs.readFileSync("index.html");
+var readerf = fs.readFileSync('index.html');
 var data = buf.toString('utf8', readerf);
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buffer = new buffer();
-  response.send("utf-8", data);
+var bfr = new buffer(data);
+  response.send('utf-8', bfr);
 });
 
 var port = process.env.PORT || 5000;
